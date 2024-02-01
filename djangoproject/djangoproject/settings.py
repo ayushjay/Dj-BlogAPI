8db5cdcaa5e0fc11f3e69e9c5a29fb3c07aa127c
ayushjay <ayushtime@gmail.com>
2024-02-01 11:37:37 +0530

    added cors

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
     "rest_framework",
+    "corsheaders",
 ]
 REST_FRAMEWORK = {  # new
     "DEFAULT_PERMISSION_CLASSES": [
@@ -50,13 +51,18 @@ REST_FRAMEWORK = {  # new
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 ROOT_URLCONF = "djangoproject.urls"
 
 TEMPLATES = [
